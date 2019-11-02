e5e5380d4c183e09b0c074a7e48ab86be1dce321
Robert Crosby <rncrosby@ucsc.edu>
2019-11-02 13:43:24 -0700

    Moved database operations to the database class

--- app.py
+++ app.py
@@ -1,153 +1,45 @@
-# Robert Crosby
-# Cody Hartsook
-# Gardner Mein
-# Contributors: Robert Crosby, Cody Hartsook, Wylie
-
 from flask import Flask, request, jsonify, make_response
 import json
+from Database import Database
 import os
 import requests
 
-keyStore = {}
+database = Database()
 
-# name our application
 app = Flask(__name__)
 
-# respond to calls to the root of our app
 @app.route("/")
 def root():
-	return "Home: CS 138: Assignment 2"
+	return "Home: CS 138: Assignment 3"
 
-# client side enpoint
-@app.route("/kv-store/<keyName>", methods=["GET", "PUT", "DELETE"])
+@app.route("/kv-store/keys/<keyName>", methods=["GET", "PUT", "DELETE"])
 def kvstore(keyName):
-	# determine node type, either main-instance or follower-instance
-	STATE = ""
+	'''
+		The main Key-Value store endpoint
 
-	if "FORWARDING_ADDRESS" in (os.environ):
-		# I am a follower process, route requests to main
-		STATE = "follower" 
-		ip_port = str(os.environ["FORWARDING_ADDRESS"]).split(":")
-
-	else:
-		# I am the main process, respond to client
-		STATE = "main"
-
-	# recieve request from client
-	if (request.method == "PUT"):
+		: GET		-> return the value for the key
+		: PUT		-> add or update a key
+		: DELETE	-> delete a key
+	'''
+	if (request.method == "GET"):
+		return database.readKey(keyName)
+	elif (request.method == "PUT"):
 		data = request.get_json()
-
-		if STATE == "main":
-			return main_storeValue(keyName, data)
-
-		else:
-			# get value from main instance keyStore/send to main
-			endpoint = 'http://' + ip_port[0] + ":" + ip_port[1] + '/kv-store/' + keyName
-			headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-			payload = json.dumps(data)
-			
-			# make recursive type call but to different ip
-			r = requests.put(endpoint, data=payload, headers=headers)
-			return make_response(r.content, r.status_code)
-
-	elif (request.method == "GET"):
-
-		if STATE == "main":
-			# get value from local keyStore
-			return main_retrieveValue(keyName)
-		else:
-			# get value from main instance keyStore/send to main
-			endpoint = 'http://' + ip_port[0] + ":" + ip_port[1] + '/kv-store/' + keyName
-			headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-			payload = keyName
-			
-		 	# make recursive type call but to different ip
-			r = requests.get(endpoint, data=payload, headers=headers)
-			return make_response(r.content, r.status_code)
-	
+		return database.insertKey(keyName, data.get("value"))
 	elif (request.method == "DELETE"):
-
-		if STATE == "main":
-			return main_deleteKey(keyName)
-		else:
-			# delete from main
-			endpoint = 'http://' + ip_port[0] + ":" + ip_port[1] + '/kv-store/' + keyName
-			headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-			payload = keyName
-			
-		 	# make recursive type call but to different ip
-			r = requests.delete(endpoint, data=payload, headers=headers)
-			return make_response(r.content, r.status_code)
-	
-def main_storeValue(key, data):
-	if len(data) < 1:
+		return database.removeKey(keyName)
+	else:
 		return jsonify({
-			"error"     : "Value is missing",
-			"message"   : "Error in PUT"
+			"error"		: "somethings not right"
 		}), 400
 
-	value = data.get("value")
-
-	if (len(value) > 50):
-		# too long
-		return jsonify({
-			"error"		: "Key is too long",
-			"message"	: "Error in PUT"
-		}), 400
-	elif key in keyStore:
-		# updated
-		keyStore[key] = value
-		return jsonify({
-			"message"	: "Updated successfully",
-			"replaced"	: True
-		}), 200
-	else:
-		keyStore[key] = value
-		return jsonify({
-			"message"	: "Added successfully",
-			"replaced"	: False
-		}), 201
-
-def main_retrieveValue(key):
-	if (key in keyStore):
-		return jsonify({
-			"doesExist"	: True,
-			"message"	: "Retrieved successfully",
-			"value"		: keyStore[key]
-		}), 200
-	else:
-		return jsonify({
-			"doesExist"	: False,
-			"error"		: "Key does not exist",
-			"message"	: "Error in GET"
-		}), 404
-
-def main_deleteKey(key):
-	if (key in keyStore):
-		del keyStore[key]
-		return jsonify({
-			"doesExist"	: True,
-			"message"	: "Deleted successfully",
-		}), 200
-	else:
-		return jsonify({
-			"doesExist"	: False,
-			"error"		: "Key does not exist",
-			"message"	: "Error in DELETE"
-		}), 404
-	
 
 
-# run the server
+
+# run the servers
 if __name__ == "__main__":
 	# run it on our localhost with a port of 8081 with debugging enabled so we can auto restart when changes are made
 	app.run(host='0.0.0.0', port=13800, debug=True)
-
-
-
-
-
-
 
 
 
