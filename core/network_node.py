81aef63b64682fe897a1c7fe8450f911ebb3704d
Cody Hartsook <chartsoo@ucsc.edu>
2019-11-03 21:28:28 -0800

    added partitioning scheme in core directory

---
+++ core/network_node.py
@@ -0,0 +1,44 @@
+# network_node.py
+# listen to requests
+
+from flask import Flask, request, jsonify, make_response
+import json
+from Database import Database
+import os
+import requests
+
+# import shard class
+from shard import consistent_hash
+shard = consistent_hash()
+
+node = Flask(__name__)
+
+@node.route("/")
+def root():
+	return "Home: CS 138: Assignment 3"
+
+@node.route("/kv-store/keys/<keyName>", methods=["GET", "PUT", "DELETE"])
+def kvstore(keyName):
+	'''
+		The main Key-Value store endpoint
+		: GET		-> return the value for the key
+		: PUT		-> add or update a key
+		: DELETE	-> delete a key
+	'''
+	if (request.method == "GET"):
+		return shard.readKey(keyName)
+	elif (request.method == "PUT"):
+		data = request.get_json()
+		return shard.insertKey(keyName, data.get("value"))
+	elif (request.method == "DELETE"):
+		return shard.removeKey(keyName)
+	else:
+		return jsonify({
+			"error"		: "somethings not right"
+		}), 400
+
+
+# run the servers
+if __name__ == "__main__":
+	# run it on our localhost with a port of 8081 with debugging enabled so we can auto restart when changes are made
+	node.run(host='0.0.0.0', port=13800, debug=True)
---
+++ core/shard.py
@@ -0,0 +1,41 @@
+# partitioning algorithm
+# implements consistent hashing 
+
+# procedure
+# 1. hash nodes then apply modulo number of nodes in system
+# 2. hash "data" then apply modulo number of nodes in system
+
+# each data packet corresponds to a node on a unit circle of integers
+# ownership of data is defined as:
+#	the predecessor on the circle
+#	the data between two nodes on the circle
+
+import xxhash
+from Database import Database # want to be able to use Database methods
+
+# create a shard given database class
+class consistent_hash(Database):
+	"""docstring for consistent_hash class"""
+	def __init__(self):
+		super.__init__(self)
+		self.peers = []
+		self.IP = None
+		self.view = None
+
+	# hash server to ring
+	# use concept of virtual node, each node is mapped to multiple places in the ring
+	# this produces further even distribution
+	def map_server():
+		pass
+
+	# hash data to ring
+	def map_data():
+		pass
+
+	# respond to view change request, perform a reshard
+	def view_change():
+		pass
+
+
+
+		
