e0d6d80d78c510347b39f0121f7f631dd7ff1af6
Cody Hartsook <chartsoo@ucsc.edu>
2019-11-11 14:47:23 -0800

    adding new branch for consistent hashing architecture

--- core/hit_endpoint.sh
+++ core/hit_endpoint.sh
@@ -6,6 +6,6 @@ addr3="10.10.0.4:13800"
 
 curl --request GET \
 	 --header 'Content-Type: application/json' \
-	 --data '{"value": "sampleValue"}'          \
 	 --write-out '%{http_code}\n' \
-	 http://127.0.0.1:13802/kv-store/keys/sampleKey
+	 http://127.0.0.1:13802/kv-store/key-count
+
