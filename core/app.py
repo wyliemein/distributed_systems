3c096348acd3dd6626c965589da12e94b777ba37
Cody Hartsook <chartsoo@ucsc.edu>
2019-11-12 10:33:28 -0800

    added key count functionality

--- core/app.py
+++ core/app.py
@@ -4,30 +4,63 @@ from flask import Flask, request, jsonify, make_response
 import json
 import os
 import requests
+import sys
 
 # import shard class
-from shard import Partition
+from Node import Node
 
 app = Flask(__name__)
+
+node = Node()
+
 @app.route("/")
 def root():
-	return "Home: CS 138: Assignment 3"
+	return node.returnInfo()
 
 # get/put/delete key for shard
 @app.route("/kv-store/keys/<keyName>", methods=["GET", "PUT", "DELETE"])
 def update_keys(keyName):
+	data = request.get_json()
 
-	# get the shard address that is storing this key, or new shard
-	key_shard = shard.key_op(keyName)
+	if (request.method == "GET"):
+		if (data is None):
+				data = {
+					"forward" : False
+				}
+		if node.containsKey(keyName):
+			return node.readKey(keyName, data["forward"])
+		else:
+			correctNode = node.hashKey(keyName)
+			data["forward"] = True
+			internal = False
+			return forward(node.others[correctNode], "/kv-store/keys/"+keyName, "GET", data, internal)
 
-	# we have the key locally
-	if (key_shard == shard.ADDRESS):
-		return exec_op(keyName)
+	elif (request.method == "PUT"):
+		if (node.keyBelongsHere(keyName)):
+			return node.insertKey(keyName, data.get("value"), ("forward" in data))
+		# key doesnt belong here
+		data["forward"] = True
+		correctNode = node.hashKey(keyName)
+		print(data, file=sys.stderr)
+		internal = False
+		return forward(node.others[correctNode], "/kv-store/keys/"+keyName, "PUT", data, internal)
 
+	elif (request.method == "DELETE"):
+		if (data is None):
+				data = {
+					"forward" : False
+				}
+		if node.containsKey(keyName):
+			return node.removeKey(keyName, data["forward"])
+		else:
+			correctNode = node.hashKey(keyName)
+			data["forward"] = True
+			internal = False
+			return forward(node.others[correctNode], "/kv-store/keys/"+keyName, "DELETE", data, internal)
 	else:
-		path = '/kv-store/keys/'+keyName
-		op = request.method
-		return forward(key_shard, path, op, keyName)
+		return jsonify({
+			"error"		: "somethings not right"
+		}), 400
 
 # get number of keys in system
 @app.route("/kv-store/key-count", methods=["GET"])
@@ -43,12 +76,13 @@ def get_key_count():
 			continue
 
 		# message node and ask them how many nodes you have
-		res = forward(node, path, op, 'key_count')
-		return make_response(res, 200)
-		node_keys = res.content
-		keys += node_keys
+		# we want forward to return response content not response
+		internal_request = True
+		res = forward(node, path, op, 'key_count', internal_request)
+		jsonResponse = json.loads(res.decode('utf-8'))
+		keys += jsonResponse['keys']
 
-	#return make_response({"keys: ":keys}, 200)
+	return make_response({"keys: ":keys}, 200)
 
 # internal messaging endpoint so that we can determine if a client or node is pinging us
 @app.route("/kv-store/internal/key-count", methods=["GET"])
@@ -56,27 +90,21 @@ def internal_key_count():
 	key_count = shard.numberOfKeys()
 
 	return jsonify({
-				"internal keys"     : key_count
+				"keys"     : key_count
 	}), 200
 
 # change our current view, repartition keys
 @app.route("/kv-store/view-change", methods=["PUT"])
 def new_view():
 	# call shard.view_change(new_view)
-	pass
+	return True
 
 # forward 
-def forward(ADDRESS, path, op, keyName):
+def forward(ADDRESS, path, op, keyName, internal):
 
 	ip_port = ADDRESS.split(":")
 	endpoint = 'http://' + ip_port[0] + ":" + ip_port[1] + path
 	headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-
-	"""return jsonify({
-				"forward"   : path,
-				"ADDRESS"   : ADDRESS,
-				"operation" : op
-	}), 200"""
 			
 	# make recursive type call but to different ip
 	# since this is a forward, add the forwarded address to response
@@ -88,6 +116,8 @@ def forward(ADDRESS, path, op, keyName):
 
 	elif op == "GET":
 		r = requests.get(endpoint, data=keyName, headers=headers)
+		if internal:
+			return r.content
 		return make_response(r.content, r.status_code)
 
 	elif op == "DELETE":
@@ -100,43 +130,18 @@ def forward(ADDRESS, path, op, keyName):
 				"message"   : "Error in forward"
 		}), 400
 
-# perform key operation with local database
-def exec_op(keyName):
-	op = request.method
-	
-	if op == "PUT":
-		data = request.get_json()
-		value = data.get("value")
-		return shard.insertKey(keyName, value)
-
-	elif op == "GET":
-		return shard.readKey(keyName)
-
-	elif op == "DELETE":
-		return shard.removeKey(keyName)
-
-	else:
-		return jsonify({
-				"error"     : "invalid requests method",
-				"message"   : "Error in exec_op"
-		}), 400
-
 
 # run the servers
 if __name__ == "__main__":
-
-	# exract view and ip
+	# exract view and ip from environment
 	VIEW_STR = os.environ["VIEW"]
-	views = VIEW_STR.split(",")
 	ADDRESS = os.environ["ADDRESS"]
-
+	node.setInfo(ADDRESS, VIEW_STR.split(","))
+	# others = VIEW_STR.split(",")
+	
+	# node.setIp(ADDRESS)
 	# create initial shard for this node, hash this shard
-	shard = Partition(ADDRESS, views)
+	# shard = Partition(ADDRESS, views)
 
 	app.run(host='0.0.0.0', port=13800, debug=True)
-
-
-
-
-
 
