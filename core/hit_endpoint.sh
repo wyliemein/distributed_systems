dc967f34ca5041eceb9471a9fe59f73d4177565d
Cody Hartsook <chartsoo@ucsc.edu>
2019-11-12 10:49:44 -0800

    adding keys and finding the key count works

--- core/hit_endpoint.sh
+++ core/hit_endpoint.sh
@@ -4,6 +4,12 @@ addr2="10.10.0.3:13800"
 addr3="10.10.0.4:13800"
 
 
+curl --request   PUT \
+     --header    "Content-Type: application/json" \
+     --write-out "\n%{http_code}\n" \
+     --data      '{"value": "sampleValue"}' \
+     http://127.0.0.1:13803/kv-store/keys/sampleKey
+
 curl --request GET \
 	 --header 'Content-Type: application/json' \
 	 --write-out '%{http_code}\n' \
