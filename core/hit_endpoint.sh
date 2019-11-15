5061afc3fde4b1d283fd07e5f7c51a1a79d01bfe
Cody Hartsook <chartsoo@ucsc.edu>
2019-11-14 22:42:36 -0800

    commit before pull

--- core/hit_endpoint.sh
+++ core/hit_endpoint.sh
@@ -8,10 +8,16 @@ curl --request   PUT \
      --header    "Content-Type: application/json" \
      --write-out "\n%{http_code}\n" \
      --data      '{"value": "sampleValue"}' \
-     http://127.0.0.1:13803/kv-store/keys/sampleKey
+     http://127.0.0.1:13802/kv-store/keys/sampleKey
 
-curl --request GET \
-	 --header 'Content-Type: application/json' \
-	 --write-out '%{http_code}\n' \
-	 http://127.0.0.1:13802/kv-store/key-count
 
+#curl --request GET \
+#	 --header 'Content-Type: application/json' \
+#	 --write-out '%{http_code}\n' \
+#	 http://127.0.0.1:13802/kv-store/key-count
+
+#curl --request PUT                                   \
+#     --header "Content-Type: application/json"       \
+#     --data      '{"view":"10.10.0.2:13800,10.10.0.3:13800,10.10.0.4:13800"}' \
+#     --write-out "%{http_code}\n"                    \
+#     http://127.0.0.1:13802/kv-store/view-change
