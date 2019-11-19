1037350d9475c6ad02452711f4687f76c40d57d9
Cody Hartsook <chartsoo@ucsc.edu>
2019-11-19 10:05:23 -0800

    cleaning

--- old/core/PUTkey.sh
+++
@@ -1,9 +0,0 @@
-let node=$1+2
-
-echo $node
-
-# curl --request   PUT                                 \
-#      --header    "Content-Type: application/json"    \
-#      --data      '{"value": "sampleValue"}'          \
-#      --write-out "%{http_code}\n"                    \
-#      http://0.0.0.0:13802/kv-store/keys/sampleKey
--- old/core/clean.sh
+++
@@ -1,9 +0,0 @@
-docker stop node1
-docker stop node2
-docker stop node3
-
-docker rm node1
-docker rm node2
-docker rm node3
-
-docker network prune 
