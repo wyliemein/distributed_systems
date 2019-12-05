6d823a9afc9bdc704f4ecf092c312ff835a5ab9f
Robert Crosby <rncrosby@ucsc.edu>
2019-12-05 14:46:00 -0800

    merge vc

--- src/vectorclock.py
+++ src/vectorclock.py
@@ -17,6 +17,30 @@ class VectorClock():
     def increment(self, index):
         self.vectorclock[index] = self.vectorclock[index] + 1
         return self
+
+    vectorclock = {}
+
+    def increment(self, index):
+        self.vectorclock[index] = self.vectorclock[index] + 1
+        return self
+
+    def merge(self, other, index):
+        if (len(self.vectorclock.items()) >= len(other.items())):
+            keys = self.vectorclock.keys()
+        else:
+            keys = other.keys()
+        t_vectorclock = {}
+        for k in keys:
+            if k not in self.vectorclock.keys():
+                self.vectorclock[k] = 0
+            if k not in other.keys():
+                other[k] = 0
+            if (self.vectorclock[k] >= other[k]):
+                t_vectorclock[k] = self.vectorclock[k]
+            else:
+                t_vectorclock[k] = other[k]
+        t_vectorclock[index] = t_vectorclock[index] + 1
+        self.vectorclock = t_vectorclock
     
     def returnClock(self):
         // NEED TO RETURN JSON OBECT
